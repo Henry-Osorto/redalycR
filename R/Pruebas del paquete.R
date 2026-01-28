70a041288f4c4f243ac46f5538b84d062b5ee3ce
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-01-27 23:46:34 -0600

    Cambio a comentarios el script de prueba del paquete

--- R/Pruebas del paquete.R
+++ R/Pruebas del paquete.R
@@ -1,18 +1,18 @@
 
 # Pruebas del paquete
 
-setwd('~/')
-library(redalycR)
+#setwd('~/')
+#library(redalycR)
 
 query <- "\"capital humano\" AND \"desarrollo economico\""
 
 
-df.muestra <- redalyc_get_articles(query, page = 1, page_size = 100)
-df <- redalyc_get_all_articles(query, page_size = 100)
+#df.muestra <- redalyc_get_articles(query, page = 1, page_size = 100)
+#df <- redalyc_get_all_articles(query, page_size = 100)
 
 #write.csv(x = df, file = 'data_redalyc.csv', row.names = F)
 #dplyr::glimpse(df)
 
-df <- read.csv('data_redalyc.csv')
+#df <- read.csv('data_redalyc.csv')
 
-df.bi <- redalyc_as_bibliometrics(df)
+#df.bi <- redalyc_as_bibliometrics(df)
